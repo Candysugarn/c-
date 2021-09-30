4e35010dea600b8240efe1788afe746fae5b28a8
Candysugarn <6406021421031@fitm.kmutnb.ac.th>
2021-09-30 21:27:08 +0700

    abc

--- GPA_A_B+_Pass.cpp
+++ GPA_A_B+_Pass.cpp
@@ -2,17 +2,17 @@
 #include <string.h>
 using namespace std;
 
-template <class T>
-T score(T men, T HW, T te, T mid, T fi);
-template <class T1>
-string Calgrade(T1 total);
-template <class T2>
-string Calgrade1(T2 total);
-template <class T3>
-string Calgrade2(T3 total);
+template <class C>
+C score(C men,C HW,C te,C mid,C final);
+template <class C1>
+string Calgrade(C1 total);
+template <class C2>
+string Calgrade1(C2 total);
+template <class C3>
+string Calgrade2(C3 total);
 int main()
 {
-    int men, HW, te, mid, fi,i;
+    int men, HW, te, mid, final,i;
     string GPA;
     for (i>1 ; i <= 20; i++)
     {
@@ -21,8 +21,8 @@ int main()
         cout << "Homework score [0-10] : "; cin >> HW;
         cout << "TestLab score [0-15] : "; cin >> te;
         cout << "Midterm score [0-35] : "; cin >> mid;
-        cout << "Finalterm score [0-35] : "; cin >> fi;
-        int total = score<int>(men, HW, te, mid, fi);
+        cout << "Finalterm score [0-35] : "; cin >> final;
+        int total = score<int>(men, HW, te, mid, final);
         string A = Calgrade(total);
         string g = Calgrade1(total);
         string s = Calgrade2(total);
@@ -34,16 +34,16 @@ int main()
     return 0;
 }
 
-template <class T>
-T score(T men, T HW, T te, T mid, T fi)
+template <class C>
+C score(C men,C HW,C te,C mid,C final)
 {
     T total;
-    total = men + HW + te + mid + fi;
+    total = men + HW + te + mid + final;
     return (total);
 }
 
-template <class T1>
-string Calgrade(T1 total)
+template <class C1>
+string Calgrade(C1 total)
 {
     string GPA;
     if (total >= 80)
@@ -62,11 +62,11 @@ string Calgrade(T1 total)
     {
         GPA = "F";
     }
-    return GPA;
+    return (GPA);
 }
 
-template <class T2>
-string Calgrade1(T2 total)
+template <class C2>
+string Calgrade1(C2 total)
 {
     string G;
     if (total >= 80)
@@ -94,11 +94,11 @@ string Calgrade1(T2 total)
     {
         G = "F";
     }
-    return G;
+    return (G);
 }
 
-template <class T3>
-string Calgrade2(T3 total)
+template <class C3>
+string Calgrade2(C3 total)
 {
     string sc;
     if (total < 50)
@@ -108,5 +108,5 @@ string Calgrade2(T3 total)
     {
         sc = "pass";
     }
-    return sc;
+    return (sc);
 } 
